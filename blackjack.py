d6f5e4ee950c7d0fa97a07b50a23e7a89abc628b
davidtc8 <davidtorresc8@gmail.com>
2020-12-04 22:23:34 -0600

    Repository: Final Code and Art.py

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,4 @@
 ############### Blackjack Project #####################
-import random
 #Difficulty Normal 😎: Use all Hints below to complete the project.
 #Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
 #Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
@@ -34,9 +33,12 @@ import random
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 #cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
+    """Returns a random card from the deck"""
     random_card = random.choice(cards)
     cards.remove(random_card)
     return random_card
@@ -48,11 +50,9 @@ computer_cards = []
 for card in range(2):
     user_cards.append(deal_card())
 
-print(f"The user has {user_cards} cards")
 
 for card in range(2):
     computer_cards.append(deal_card())
-print(f"The computer has {computer_cards} cards")
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input
 #and returns the score.
@@ -70,38 +70,77 @@ def calculate_score(total_user, total_computer):
         print('The computer has won!')
     elif total_user == 21:
         print('you have won!')
-    elif total_user <= 21:
-        decision = input("Would you like another card? (type yes or no): ").lower()
-        if decision == 'yes':
-            for card in user_cards:
-                card_given = deal_card()
-                user_cards.append(card_given)
-                print(f"You received a {card_given}")
-                total_user += card_given
-                print(f"The user now has {total_user}")
-                break
-            ace = 11
-            blackjack = 21
-            if ace in user_cards and total_user > blackjack:
-                user_cards.remove(ace)
-                user_cards.append(1)
-                total_user = 0
-                for card in user_cards:
-                    total_user += card
-                print(f"You converted the {ace} to 1, and the total you have is: {total_user}")
-            elif total_user > blackjack:
-                print('You lose!')
-        else:
-            print("The game has ended")
-    elif total_computer <= 17:
-        for card in computer_cards:
-            next_computer_card = deal_card()
-            computer_cards.append(next_computer_card)
-            total_computer += card_given
-            continue
-    return f"The user has: {total_user}\nComputer has: {total_computer}"
+    return f"The user has: {user_cards}, user score: {total_user}\nComputer has: {computer_cards[0]} and another random card, Computer score: {computer_cards[0]}"
 
 print(calculate_score(total_user, total_computer))
+
+def final_score(total_user, total_computer):
+    for card in user_cards:
+        total_user += card
+    for card in computer_cards:
+        total_computer += card
+    return f"The user has: {total_user}\nComputer has: {total_computer}"
+
+if sum(user_cards) < 21 and sum(computer_cards) < 21:
+    decision = input("Would you like another card? (type yes or no): ").lower()
+    if decision == 'yes':
+        for card in user_cards:
+            card_given = deal_card()
+            user_cards.append(card_given)
+            print(f"You received a {card_given}")
+            break
+        ace = 11
+        blackjack = 21
+        if ace in user_cards and total_user > blackjack:
+            user_cards.remove(ace)
+            user_cards.append(1)
+            total_user = 0
+            for card in user_cards:
+                total_user += card
+            print(f"You converted the {ace} to 1, and the total you have is: {total_user}")
+elif sum(computer_cards) == 21:
+    decision = 0
+    print("Game Over!")
+else:
+    decision = 0
+    print("Game Over!")
+
+if sum(computer_cards) <= 17 and decision != 0:
+    for card in computer_cards:
+        next_computer_card = deal_card()
+        print(f"The computer decided to take another card ({next_computer_card})")
+        computer_cards.append(next_computer_card)
+        break
+
+print(final_score(total_user, total_computer))
+
+def compare():
+    if sum(computer_cards) > 21:
+        print(f"The computer loses because he has {sum(computer_cards)}")
+        if sum(user_cards) > 21:
+            print("Both you and the computer lost")
+        else:
+            print('you have won!')
+    elif sum(user_cards) > 21:
+        print(f"You lost because you have {sum(user_cards)}")
+        if sum(computer_cards) > 21:
+            print("Both you and the computer lost")
+        else:
+            print('The computer has won!')
+    elif sum(user_cards) == sum(computer_cards):
+        print("It's a draw")
+    elif sum(computer_cards) == 21:
+        print('The computer has won!')
+    elif sum(user_cards) == 21:
+        print('you have won!')
+    elif sum(user_cards) > sum(computer_cards) and sum(user_cards) < 21:
+        print('you have won!')
+    elif sum(computer_cards) > sum(user_cards) and sum(computer_cards) < 21:
+        print('The computer has won!')
+
+if decision != 0:
+    compare()
+
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
